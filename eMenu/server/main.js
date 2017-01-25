e841e956bccc1ab02fb2984d37a183ca08c33496
Unknown <julio.romotishca@gmail.com>
2017-01-25 10:46:54 -0600

    Mejorando un poco
    :D utilizando conexion socket

--- eMenu/server/main.js
+++ eMenu/server/main.js
@@ -1,7 +1,17 @@
 var express = require('express');
-var router = express.Router();
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 var mysql = require('mysql');
-/* GET home page. */
+
+app.use(express.static('public'));
+
+app.get('/admin',function(req,res){
+  
+  console.log('intentan acceder al admin');
+});
+//--------------------------------------------
+//        sql
 var connection = mysql.createConnection({
    host: 'localhost',
    user: 'root',
@@ -19,6 +29,8 @@ connection.connect(function(error){
 });
 
 //-------------------------------------------------------------
+io.on('connection',function(socket){
+
 var query = connection.query('SELECT max(idmenu) AS idmax FROM menu', function(error, result){
       if(error){
          throw error;
@@ -27,8 +39,8 @@ var query = connection.query('SELECT max(idmenu) AS idmax FROM menu', function(e
          if(resultado.length > 0){
            console.log('numero de reg:',resultado[0].idmax);
           var n=resultado[0].idmax;
-          for (var i = 1; i <n+1; i++) {
-            var query = connection.query('SELECT * FROM menu WHERE idmenu = ?', [i], function(error, result){
+          for (var i = 0; i <n; i++) {
+            var query = connection.query('SELECT * FROM menu WHERE idmenu = ?', [i+1], function(error, result){
                   if(error){
                      throw error;
                   }else{
@@ -36,10 +48,8 @@ var query = connection.query('SELECT max(idmenu) AS idmax FROM menu', function(e
                      if(resultado.length > 0){
                        console.log(n,i);
                         console.log(resultado[0].idmenu,resultado[0].nombre,resultado[0].descripcion,resultado[0].precio,resultado[0].imagen);
-                        router.get('/', function(req, res) {
-                          res.render('index', { id: resultado[0].idmenu,nombre: resultado[0].nombre,descripcion: resultado[0].descripcion, precio: resultado[0].precio , imge: resultado[0].imagen });
+                        socket.emit('menu',{id: resultado[0].idmenu,nombre: resultado[0].nombre,descripcion: resultado[0].descripcion,precio: resultado[0].precio,imagen: resultado[0].imagen
                         });
-
                      }else{
                         console.log('Registro no encontrado');
                      }
@@ -56,12 +66,11 @@ var query = connection.query('SELECT max(idmenu) AS idmax FROM menu', function(e
 
 );
 
-//-------------------------------------------------------------------
+});
+
+//--------------------------------------------
 
 
-
-
-
-
-
-module.exports = router;
+server.listen(8080,function() {
+  console.log("Servidor corriendo en http://localhost:8080");
+});
